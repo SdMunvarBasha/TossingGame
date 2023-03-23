40349f5281650c2df64a9049976582476f7ab4e6
Syed Munvar Basha <124273008+SdMunvarBasha@users.noreply.github.com>
2023-03-23 23:50:02 +0530

    Create output.java

---
+++ output.java
@@ -0,0 +1,17 @@
+cycle1 
+ [rounds, player1, player2] 
+ [[round1, heads, heads], [round2, Tails, Tails], [round3, heads, heads], [round4, Tails, Tails], [round5, Tails, heads], [round6, heads, heads], [round7, Tails, Tails], [round8, Tails, Tails], [round9, heads, Tails], [round10, Tails, heads]] 
+ player2 wins 
+ cycle2 
+ [rounds, player1, player2] 
+ [[round1, heads, heads], [round2, Tails, Tails], [round3, heads, heads], [round4, Tails, Tails], [round5, Tails, heads], [round6, heads, heads], [round7, Tails, Tails], [round8, Tails, Tails], [round9, heads, Tails], [round10, Tails, heads], [round1, heads, Tails], [round2, heads,  
+ Tails], [round3, heads, heads], [round4, heads, heads], [round5, Tails, heads], [round6, Tails, heads], [round7, Tails, heads], [round8, heads, heads], [round9, Tails, heads], [round10, heads, Tails]] 
+ player2 wins 
+ cycle3 
+ [rounds, player1, player2] 
+ [[round1, heads, heads], [round2, Tails, Tails], [round3, heads, heads], [round4, Tails, Tails], [round5, Tails, heads], [round6, heads, heads], [round7, Tails, Tails], [round8, Tails, Tails], [round9, heads, Tails], [round10, Tails, heads], [round1, heads, Tails], [round2, heads,  
+ Tails], [round3, heads, heads], [round4, heads, heads], [round5, Tails, heads], [round6, Tails, heads], [round7, Tails, heads], [round8, heads, heads], [round9, Tails, heads], [round10, heads, Tails], [round1, Tails, Tails], [round2, heads, Tails], [round3, Tails, heads], [round4,  
+ heads, heads], [round5, Tails, heads], [round6, Tails, heads], [round7, Tails, Tails], [round8, heads, Tails], [round9, Tails, heads], [round10, Tails, Tails]] 
+ player2 wins 
+ the final winner after 3 rounds is 
+ player2 wins
